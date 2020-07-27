add9f796ea6cbdec8746ef9d26a2dcf078f102a4
Alexandr Ovcharenko <ovcharenko.messor@gmail.com>
2020-07-27 12:41:05 +0300

    password generator bot with builder pattern works correctly add battons to tg bot

--- src/main/java/Bot.java
+++ src/main/java/Bot.java
@@ -11,7 +11,18 @@ public class Bot extends TelegramLongPollingBot {
     private static final String botUserName = "@PasswordGeneratorBot_bot:";
     private static final String token = "1187477046:AAH_XIotZRwDUMOuKIq7-4iKvW78nDYlDSE";
     ReplyKeyboardMarkup replyKeyboardMarkup = new ReplyKeyboardMarkup();
-    PasswordGenerator passwordGenerator = new PasswordGenerator(20);
+    PasswordGenerator passwordGeneratorWithDigitsAndPunctuation = new PasswordGenerator.PasswordGeneratorBuilder()
+            .useLower(true)
+            .useUpper(true)
+            .useDigits(true)
+            .usePunctuation(true)
+            .build();
+    PasswordGenerator passwordGeneratorWithoutDigitsAndPunctuation = new PasswordGenerator.PasswordGeneratorBuilder()
+            .useLower(true)
+            .useUpper(true)
+            .useDigits(false)
+            .usePunctuation(false)
+            .build();
     long chat_id;
 
     @Override
@@ -41,38 +52,194 @@ public class Bot extends TelegramLongPollingBot {
         }
     }
 
-//    public Object input(String msg){
-//        if(msg.contains("Create a password")){
-//            return getPassword();
-//        }
-//        return msg;
-//    }
-
-    public String getPassword() {
-        return passwordGenerator.passwordGenerator();
-    }
-
     public String getMessage(String msg) {
         ArrayList<KeyboardRow> keyboard = new ArrayList<>();
         KeyboardRow keyboardFirstRow = new KeyboardRow();
+        KeyboardRow keyboardSecondRow = new KeyboardRow();
+        KeyboardRow keyboardThirdRow = new KeyboardRow();
+        KeyboardRow keyboardForthRow = new KeyboardRow();
+        KeyboardRow keyboardFivesRow = new KeyboardRow();
+        KeyboardRow keyboardSixRow = new KeyboardRow();
+        KeyboardRow keyboardSevensRow = new KeyboardRow();
+        KeyboardRow keyboardEightsRow = new KeyboardRow();
+        KeyboardRow keyboardNinesRow = new KeyboardRow();
+
 
         replyKeyboardMarkup.setSelective(true);
         replyKeyboardMarkup.setResizeKeyboard(true);
         replyKeyboardMarkup.setOneTimeKeyboard(true);
 
-        if(msg.equals("Привет") || msg.equals("Меню") || msg.equals("/start")){
-            //keyboard.clear();
+        if(msg.equals("/start")){
             keyboardFirstRow.add("Create");
             keyboard.add(keyboardFirstRow);
             replyKeyboardMarkup.setKeyboard(keyboard);
-            return "Выберите пункт меню";
+            return "Choose";
         }
         if (msg.equals("Create")) {
-            try {
-                return getPassword();
-            } catch (Exception e) {
-                e.printStackTrace();
-            }
+            keyboard.clear();
+            keyboardFirstRow.add("With Digits and Punctuation");
+            keyboardSecondRow.add("Only with letters");
+            keyboard.add(keyboardFirstRow);
+            keyboard.add(keyboardSecondRow);
+            replyKeyboardMarkup.setKeyboard(keyboard);
+            return "Select with what password";
+        }
+        if (msg.equals("With Digits and Punctuation")) {
+            keyboard.clear();
+            keyboardFirstRow.add("8 symbols");
+            keyboardSecondRow.add("9 symbols");
+            keyboardThirdRow.add("10 symbols");
+            keyboardForthRow.add("11 symbols");
+            keyboardFivesRow.add("12 symbols");
+            keyboardSixRow.add("13 symbols");
+            keyboardSevensRow.add("14 symbols");
+            keyboardEightsRow.add("15 symbols");
+            keyboardNinesRow.add("16 symbols");
+            keyboard.add(keyboardFirstRow);
+            keyboard.add(keyboardSecondRow);
+            keyboard.add(keyboardThirdRow);
+            keyboard.add(keyboardForthRow);
+            keyboard.add(keyboardFivesRow);
+            keyboard.add(keyboardSixRow);
+            keyboard.add(keyboardSevensRow);
+            keyboard.add(keyboardEightsRow);
+            keyboard.add(keyboardNinesRow);
+            replyKeyboardMarkup.setKeyboard(keyboard);
+            return "Select dimension";
+        }
+        switch (msg) {
+            case "8 symbols":
+                try {
+                    return passwordGeneratorWithDigitsAndPunctuation.generate(8);
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            case "9 symbols":
+                try {
+                    return passwordGeneratorWithDigitsAndPunctuation.generate(9);
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            case "10 symbols":
+                try {
+                    return passwordGeneratorWithDigitsAndPunctuation.generate(10);
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            case "11 symbols":
+                try {
+                    return passwordGeneratorWithDigitsAndPunctuation.generate(11);
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            case "12 symbols":
+                try {
+                    return passwordGeneratorWithDigitsAndPunctuation.generate(12);
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            case "13 symbols":
+                try {
+                    return passwordGeneratorWithDigitsAndPunctuation.generate(13);
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            case "14 symbols":
+                try {
+                    return passwordGeneratorWithDigitsAndPunctuation.generate(14);
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            case "15 symbols":
+                try {
+                    return passwordGeneratorWithDigitsAndPunctuation.generate(15);
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            case "16 symbols":
+                try {
+                    return passwordGeneratorWithDigitsAndPunctuation.generate(16);
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+        }
+        if (msg.equals("Only with letters")) {
+            keyboardFirstRow.add("8 symbols");
+            keyboardSecondRow.add("9 symbols");
+            keyboardThirdRow.add("10 symbols");
+            keyboardForthRow.add("11 symbols");
+            keyboardFivesRow.add("12 symbols");
+            keyboardSixRow.add("13 symbols");
+            keyboardSevensRow.add("14 symbols");
+            keyboardEightsRow.add("15 symbols");
+            keyboardNinesRow.add("16 symbols");
+            keyboard.add(keyboardFirstRow);
+            keyboard.add(keyboardSecondRow);
+            keyboard.add(keyboardThirdRow);
+            keyboard.add(keyboardForthRow);
+            keyboard.add(keyboardFivesRow);
+            keyboard.add(keyboardSixRow);
+            keyboard.add(keyboardSevensRow);
+            keyboard.add(keyboardEightsRow);
+            keyboard.add(keyboardNinesRow);
+            replyKeyboardMarkup.setKeyboard(keyboard);
+            return "Select dimension";
+        }
+        switch (msg) {
+            case "8 symbols":
+                try {
+                    return passwordGeneratorWithoutDigitsAndPunctuation.generate(8);
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            case "9 symbols":
+                try {
+                    return passwordGeneratorWithoutDigitsAndPunctuation.generate(9);
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            case "10 symbols":
+                try {
+                    return passwordGeneratorWithoutDigitsAndPunctuation.generate(10);
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            case "11 symbols":
+                try {
+                    return passwordGeneratorWithoutDigitsAndPunctuation.generate(11);
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            case "12 symbols":
+                try {
+                    return passwordGeneratorWithoutDigitsAndPunctuation.generate(12);
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            case "13 symbols":
+                try {
+                    return passwordGeneratorWithoutDigitsAndPunctuation.generate(13);
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            case "14 symbols":
+                try {
+                    return passwordGeneratorWithoutDigitsAndPunctuation.generate(14);
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            case "15 symbols":
+                try {
+                    return passwordGeneratorWithoutDigitsAndPunctuation.generate(15);
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            case "16 symbols":
+                try {
+                    return passwordGeneratorWithoutDigitsAndPunctuation.generate(16);
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
         }
         return "Something wrong";
     }
--- src/main/java/PasswordGenerator.java
+++ src/main/java/PasswordGenerator.java
@@ -1,40 +1,156 @@
-import java.security.SecureRandom;
 import java.util.ArrayList;
-import java.util.Collections;
+import java.util.List;
+import java.util.Random;
 
-public class PasswordGenerator {
-    private final int Characters;
-    private SecureRandom scrand;
-    private ArrayList<Character> valid;
+public final class PasswordGenerator {
+//    private final int Characters;
+//    private SecureRandom scrand;
+//    private ArrayList<Character> valid;
+//
+//    public PasswordGenerator(int n) {
+//        this.Characters = n;
+//    }
+//    public char randChar() {
+//        return this.valid.get(scrand.nextInt(this.valid.size()));
+//    }
+//
+//    public String passwordGenerator() {
+//        scrand = new SecureRandom();
+//        valid = new ArrayList<>();
+//        //Filling the ArrayList with the characters we want to use based on ascii table:
+//        for (int i = 65; i < 91; i++) {
+//            valid.add((char) i);         //A-Z
+//            valid.add((char) (i+32));    //a-z
+//        }
+//        for (int i = 48; i < 58; i++) {
+//            valid.add((char) i);
+//        }
+//        for (int i = 35; i < 39; i++) {
+//            valid.add((char) i);
+//        }
+//        valid.add((char) 64);
+//        Collections.shuffle(valid);
+//
+//        StringBuilder stringBuilder = new StringBuilder();
+//        for (int i = 0; i < Characters; i++) {
+//            stringBuilder.append(randChar());
+//        }
+//        return stringBuilder.toString();
+//    }
+private static final String LOWER = "abcdefghijklmnopqrstuvwxyz";
+    private static final String UPPER = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    private static final String DIGITS = "0123456789";
+    private static final String PUNCTUATION = "!@#$%&*()_+-=[]|,./?><";
+    private final boolean useLower;
+    private final boolean useUpper;
+    private final boolean useDigits;
+    private final boolean usePunctuation;
 
-    public PasswordGenerator(int n) {
-        this.Characters = n;
+    private PasswordGenerator(PasswordGeneratorBuilder builder) {
+        this.useLower = builder.useLower;
+        this.useUpper = builder.useUpper;
+        this.useDigits = builder.useDigits;
+        this.usePunctuation = builder.usePunctuation;
     }
-    public char randChar() {
-        return this.valid.get(scrand.nextInt(this.valid.size()));
+
+    public static class PasswordGeneratorBuilder {
+
+        private boolean useLower;
+        private boolean useUpper;
+        private boolean useDigits;
+        private boolean usePunctuation;
+
+        public PasswordGeneratorBuilder() {
+            this.useLower = false;
+            this.useUpper = false;
+            this.useDigits = false;
+            this.usePunctuation = false;
+        }
+
+        public PasswordGeneratorBuilder useLower(boolean useLower) {
+            this.useLower = useLower;
+            return this;
+        }
+
+        public PasswordGeneratorBuilder useUpper(boolean useUpper) {
+            this.useUpper = useUpper;
+            return this;
+        }
+
+        public PasswordGeneratorBuilder useDigits(boolean useDigits) {
+            this.useDigits = useDigits;
+            return this;
+        }
+
+        public PasswordGeneratorBuilder usePunctuation(boolean usePunctuation) {
+            this.usePunctuation = usePunctuation;
+            return this;
+        }
+
+        public PasswordGenerator build() {
+            return new PasswordGenerator(this);
+        }
     }
 
-    public String passwordGenerator() {
-        scrand = new SecureRandom();
-        valid = new ArrayList<>();
-        //Filling the ArrayList with the characters we want to use based on ascii table:
-        for (int i = 65; i < 91; i++) {
-            valid.add((char) i);         //A-Z
-            valid.add((char) (i+32));    //a-z
+    public String generate(int length) {
+        // Argument Validation.
+        if (length <= 0) {
+            return "";
         }
-        for (int i = 48; i < 58; i++) {
-            valid.add((char) i);
-        }
-        for (int i = 35; i < 39; i++) {
-            valid.add((char) i);
-        }
-        valid.add((char) 64);
-        Collections.shuffle(valid);
 
-        StringBuilder stringBuilder = new StringBuilder();
-        for (int i = 0; i < Characters; i++) {
-            stringBuilder.append(randChar());
+        // Variables.
+        StringBuilder password = new StringBuilder(length);
+        Random random = new Random(System.nanoTime());
+
+        // Collect the categories to use.
+        List<String> charCategories = new ArrayList<>(4);
+        if (useLower) {
+            charCategories.add(LOWER);
         }
-        return stringBuilder.toString();
+        if (useUpper) {
+            charCategories.add(UPPER);
+        }
+        if (useDigits) {
+            charCategories.add(DIGITS);
+        }
+        if (usePunctuation) {
+            charCategories.add(PUNCTUATION);
+        }
+
+        // Build the password.
+        for (int i = 0; i < length; i++) {
+            String charCategory = charCategories.get(random.nextInt(charCategories.size()));
+            int position = random.nextInt(charCategory.length());
+            password.append(charCategory.charAt(position));
+        }
+        return new String(password);
     }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
